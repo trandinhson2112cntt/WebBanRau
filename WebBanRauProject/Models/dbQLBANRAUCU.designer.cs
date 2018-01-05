101eaf47c9033f1b6c8e398bbe4fb8eb9500257f
THUHIEN <nguyenthithuhien1104cntt@gmail.com>
2018-01-05 11:22:41 +0700

    Fix

--- WebBanRauProject/Models/dbQLBANRAUCU.designer.cs
+++ WebBanRauProject/Models/dbQLBANRAUCU.designer.cs
@@ -51,10 +51,13 @@ namespace WebBanRauProject.Models
     partial void InsertNHACUNGCAP(NHACUNGCAP instance);
     partial void UpdateNHACUNGCAP(NHACUNGCAP instance);
     partial void DeleteNHACUNGCAP(NHACUNGCAP instance);
+    partial void InsertQuanLyGoiYMonAn(QuanLyGoiYMonAn instance);
+    partial void UpdateQuanLyGoiYMonAn(QuanLyGoiYMonAn instance);
+    partial void DeleteQuanLyGoiYMonAn(QuanLyGoiYMonAn instance);
     #endregion
 		
 		public dbQLBANRAUCUDataContext() : 
-				base(global::System.Configuration.ConfigurationManager.ConnectionStrings["QLBANRAUCUConnectionString2"].ConnectionString, mappingSource)
+				base(global::System.Configuration.ConfigurationManager.ConnectionStrings["QLBANRAUCUConnectionString3"].ConnectionString, mappingSource)
 		{
 			OnCreated();
 		}
@@ -144,14 +147,6 @@ namespace WebBanRauProject.Models
 			get
 			{
 				return this.GetTable<QuanLyGoiYMonAn>();
-			}
-		}
-		
-		public System.Data.Linq.Table<QuanLyTinTuc> QuanLyTinTucs
-		{
-			get
-			{
-				return this.GetTable<QuanLyTinTuc>();
 			}
 		}
 	}
@@ -1632,8 +1627,10 @@ namespace WebBanRauProject.Models
 	}
 	
 	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.QuanLyGoiYMonAn")]
-	public partial class QuanLyGoiYMonAn
+	public partial class QuanLyGoiYMonAn : INotifyPropertyChanging, INotifyPropertyChanged
 	{
+		
+		private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
 		
 		private int _MASO;
 		
@@ -1641,11 +1638,28 @@ namespace WebBanRauProject.Models
 		
 		private string _MOTA;
 		
+		private string _HINHANH;
+		
+    #region Extensibility Method Definitions
+    partial void OnLoaded();
+    partial void OnValidate(System.Data.Linq.ChangeAction action);
+    partial void OnCreated();
+    partial void OnMASOChanging(int value);
+    partial void OnMASOChanged();
+    partial void OnTENMONANChanging(string value);
+    partial void OnTENMONANChanged();
+    partial void OnMOTAChanging(string value);
+    partial void OnMOTAChanged();
+    partial void OnHINHANHChanging(string value);
+    partial void OnHINHANHChanged();
+    #endregion
+		
 		public QuanLyGoiYMonAn()
 		{
+			OnCreated();
 		}
 		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_MASO", AutoSync=AutoSync.Always, DbType="Int NOT NULL IDENTITY", IsDbGenerated=true)]
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_MASO", AutoSync=AutoSync.OnInsert, DbType="Int NOT NULL IDENTITY", IsPrimaryKey=true, IsDbGenerated=true)]
 		public int MASO
 		{
 			get
@@ -1656,7 +1670,11 @@ namespace WebBanRauProject.Models
 			{
 				if ((this._MASO != value))
 				{
+					this.OnMASOChanging(value);
+					this.SendPropertyChanging();
 					this._MASO = value;
+					this.SendPropertyChanged("MASO");
+					this.OnMASOChanged();
 				}
 			}
 		}
@@ -1672,7 +1690,11 @@ namespace WebBanRauProject.Models
 			{
 				if ((this._TENMONAN != value))
 				{
+					this.OnTENMONANChanging(value);
+					this.SendPropertyChanging();
 					this._TENMONAN = value;
+					this.SendPropertyChanged("TENMONAN");
+					this.OnTENMONANChanged();
 				}
 			}
 		}
@@ -1688,89 +1710,52 @@ namespace WebBanRauProject.Models
 			{
 				if ((this._MOTA != value))
 				{
+					this.OnMOTAChanging(value);
+					this.SendPropertyChanging();
 					this._MOTA = value;
+					this.SendPropertyChanged("MOTA");
+					this.OnMOTAChanged();
 				}
 			}
 		}
-	}
-	
-	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.QuanLyTinTuc")]
-	public partial class QuanLyTinTuc
-	{
 		
-		private int _MATIN;
-		
-		private string _TIEUDE;
-		
-		private string _NOIDUNG;
-		
-		private System.Nullable<System.DateTime> _NGAYDANGTIN;
-		
-		public QuanLyTinTuc()
-		{
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_MATIN", AutoSync=AutoSync.Always, DbType="Int NOT NULL IDENTITY", IsDbGenerated=true)]
-		public int MATIN
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_HINHANH", DbType="VarChar(30)")]
+		public string HINHANH
 		{
 			get
 			{
-				return this._MATIN;
+				return this._HINHANH;
 			}
 			set
 			{
-				if ((this._MATIN != value))
+				if ((this._HINHANH != value))
 				{
-					this._MATIN = value;
+					this.OnHINHANHChanging(value);
+					this.SendPropertyChanging();
+					this._HINHANH = value;
+					this.SendPropertyChanged("HINHANH");
+					this.OnHINHANHChanged();
 				}
 			}
 		}
 		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_TIEUDE", DbType="NVarChar(50)")]
-		public string TIEUDE
+		public event PropertyChangingEventHandler PropertyChanging;
+		
+		public event PropertyChangedEventHandler PropertyChanged;
+		
+		protected virtual void SendPropertyChanging()
 		{
-			get
+			if ((this.PropertyChanging != null))
 			{
-				return this._TIEUDE;
-			}
-			set
-			{
-				if ((this._TIEUDE != value))
-				{
-					this._TIEUDE = value;
-				}
+				this.PropertyChanging(this, emptyChangingEventArgs);
 			}
 		}
 		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_NOIDUNG", DbType="NVarChar(MAX)")]
-		public string NOIDUNG
+		protected virtual void SendPropertyChanged(String propertyName)
 		{
-			get
+			if ((this.PropertyChanged != null))
 			{
-				return this._NOIDUNG;
-			}
-			set
-			{
-				if ((this._NOIDUNG != value))
-				{
-					this._NOIDUNG = value;
-				}
-			}
-		}
-		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_NGAYDANGTIN", DbType="DateTime")]
-		public System.Nullable<System.DateTime> NGAYDANGTIN
-		{
-			get
-			{
-				return this._NGAYDANGTIN;
-			}
-			set
-			{
-				if ((this._NGAYDANGTIN != value))
-				{
-					this._NGAYDANGTIN = value;
-				}
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
 			}
 		}
 	}
